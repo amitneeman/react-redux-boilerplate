fcc64081a2fe595c5ea5bbe2377c248f198acc99
Amit Neeman <amitneeman@Amits-MacBook-Pro.local>
2018-04-01 21:30:29 +0300

    fixed the clean script

--- internals/scripts/clean.js
+++ internals/scripts/clean.js
@@ -1,6 +1,12 @@
 var shell = require('shelljs');
 shell.config.silent = true;
 
-shell.exec('rm -rf ./git');
+var output = shell.exec('rm -rf .git');
+
+if(output.stderr){
+    console.log("failed to remove the old git repository: ");
+    console.log(output.stderr);
+    return;
+}
 
 console.log('Removed the boilerplate git repo');
