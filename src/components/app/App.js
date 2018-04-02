5a25cbac34adbc2a7194d9536593b63f66d8ffd8
Amit Neeman <amitneeman@Amits-MacBook-Pro.local>
2018-04-02 16:00:08 +0300

    touch

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styles from './App.styles.js';
 
+import Container from '../../containers/container/Container';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Container />
         <h1 style={styles.title}>React Redux boilerplate</h1>
       </div>
     );
