00ea01c7afca4722f130d5380818f0053735e598
Amit Neeman <amitneeman01@gmail.com>
2018-06-09 21:49:31 +0300

    added a homepage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,16 +3,31 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   color: ${props => props.color ? props.color : 'black'};
+  transition: all 0.5s ease;
   &:hover {
-    color: black;
-  }
+    color: red;
+  };
+  display: flex;
+  align-items: center;
+  flex-direction: column
 `;
+
+const Section = styled.section`
+  font-size: 20p5;
+`;
+
+
 
 class App extends Component {
   render() {
     return (
       <Container>
-        Hello
+        <h1>React-redux boilerplate</h1>
+        <Section>
+          <div><b>npm run comp COMPONENT_NAME</b> will generate a component</div>  
+          <div><b>npm run cont CONTAINER_NAME</b> will generate a container</div>    
+          <div><b>npm run clean</b> will eject from the boilerplate git repository</div>
+        </Section>
       </Container>
     );
   }
